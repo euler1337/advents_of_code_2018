b7ee1fc4e1e67e2031653b65f4d2a25f0e349520
ext.m.andersson <ext.m.andersson@king.com>
2018-12-06 15:18:29 +0100

    2b finished

--- 2/hello.go
+++ 2/hello.go
@@ -12,10 +12,7 @@ func check(e error) {
     }
 }
 
-
-func main() {
-	
-
+func calc1() {
 	file, err := os.Open("input.txt")
 	check(err)
 	defer file.Close()
@@ -63,4 +60,64 @@ func main() {
 		}
 	}
 	fmt.Printf("2 count [%d], 3 count [%d], mult [%d] \n", count2, count3, count2*count3)
+}
+
+func CalcNumberOfDifferentChars(e1 string ,  e2 string) (int, int) {
+	l1 := len(e1)
+	l2 := len(e2)
+
+
+
+	if l1 != l2 {
+		return -1, 0
+	}
+
+	diffIndex := 0
+	diffCount := 0
+	for index := range e1 {
+		if e1[index] != e2[index] {
+			diffCount+=1
+			diffIndex = index
+		}
+	}
+
+	return diffCount, diffIndex
+}
+
+func calc2() {
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	// Find number of rows which contain a letter that occurs exactly twice
+	// Find number of rows which contain a letter that occurs exactly three times
+	// Multiply the
+
+	inputs := make([]string, 0)
+
+	for scanner.Scan() {
+		input_str := scanner.Text()
+		inputs = append(inputs, input_str)
+	}
+
+	for _, e1 := range inputs {
+		for _, e2 := range inputs {
+			diffCount, diffIndex := CalcNumberOfDifferentChars(e1, e2)
+			if diffCount == 1 {
+				fmt.Printf("FOUND ONE: e1 = %s, e2 = %s \n", e1, e2)
+				fmt.Printf("Index: %d, ANSWER ---------> %s%s <-------------\n", diffIndex, e1[0:diffIndex], e1[diffIndex+1:])
+			}
+		}
+	}
+
+
+
+}
+
+
+func main() {
+	//calc1()
+	calc2()
+
 }
